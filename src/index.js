3e2de3ca231a8bb5c99854077b181f0e30f8b8c3
Volodymyr Anisimov <144102084+vanisimov4@users.noreply.github.com>
2025-07-04 22:01:33 +0300

    expressTest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-const message = "Hello node";
+// src/index.js
+//
+// 8LM3u4VSPa0gc1Yj
 
-console.log(message);
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+// app.use('*', (req, res, next) => {
+//   res.status(404).json({
+//     message: 'Not found',
+//   });
+// });
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
